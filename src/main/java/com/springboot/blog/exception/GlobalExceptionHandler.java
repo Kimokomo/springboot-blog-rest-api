c71b891f15f7ec05ba0441b81141006c99e14299
YassinK <karim.yassin@hotmail.com>
2023-09-24 13:31:03 +0200

    Customizing Validation Response

--- src/main/java/com/springboot/blog/exception/GlobalExceptionHandler.java
+++ src/main/java/com/springboot/blog/exception/GlobalExceptionHandler.java
@@ -1,15 +1,22 @@
 package com.springboot.blog.exception;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import com.springboot.blog.payload.ErrorDetails;
 
 @ControllerAdvice  // We use this annotation to handle Exceptions globally
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
   // handle specific exceptions
   @ExceptionHandler(ResourceNotFoundException.class)
@@ -30,5 +37,32 @@ public class GlobalExceptionHandler {
     ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(), webRequest.getDescription(false));
     return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
   }
+
+  // Customizing Validation Response  First approach
+  // In this approach we extend from ResponseEntityExceptionHandler and the override the method
+
+  @Override
+  protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+    Map<String, String> errors = new HashMap<>();
+    ex.getBindingResult().getAllErrors().forEach((error) -> {
+      String fieldName = ((FieldError) error).getField();
+      String message = error.getDefaultMessage();
+      errors.put(fieldName, message);
+    });
+    return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+  }
+
+  // Customizing Validation Response Second approach
+  // Here we dont need to extend from ResponseEntityExceptionHandler (error otherwise)
+  //  @ExceptionHandler(MethodArgumentNotValidException.class)
+  //  public ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException exception, WebRequest webRequest) {
+  //    Map<String, String> errors = new HashMap<>();
+  //    exception.getBindingResult().getAllErrors().forEach((error) -> {
+  //      String fieldName = ((FieldError) error).getField();
+  //      String message = error.getDefaultMessage();
+  //      errors.put(fieldName, message);
+  //    });
+  //    return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+  //  }
 
 }
